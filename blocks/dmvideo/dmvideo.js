c4d3620f478bfe884f1a8b656ec967db788c0eb1
xiaoyu-chen <xiaoyu.chen@vodafone.com>
2024-05-22 17:07:10 +0100

    wip

--- blocks/dmvideo/dmvideo.js
+++ blocks/dmvideo/dmvideo.js
@@ -3,92 +3,36 @@
  * Show a video referenced by a link
  * https://www.hlx.live/developer/block-collection/video
  */
-
-function embedYoutube(url, autoplay) {
-  const usp = new URLSearchParams(url.search);
-  const suffix = autoplay ? '&muted=1&autoplay=1' : '';
-  let vid = usp.get('v') ? encodeURIComponent(usp.get('v')) : '';
-  const embed = url.pathname;
-  if (url.origin.includes('youtu.be')) {
-    [, vid] = url.pathname.split('/');
-  }
-  return `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-      <iframe src="https://www.youtube.com${vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
-      allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" allowfullscreen="" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
-    </div>`;
+function loadVideoEmbed(block) {
+  console.log(block.classList);
+  return `
+    <div class="dm-video-embed">
+      <style type="text/css">
+      #s7video_div.s7videoviewer{
+        width:100%; 
+        height:auto;
+      }
+    </style>
+    <script type="text/javascript" src="https://s7g10.scene7.com/s7viewers/html5/js/VideoViewer.js"></script>
+    <div id="s7video_div" class="video_social"></div>
+    <script type="text/javascript">
+      var s7videoviewer = new s7viewers.VideoViewer({
+        "containerId" : "s7video_div",
+        "params" : { 
+          "serverurl" : "https://s7g10.scene7.com/is/image/",
+          "contenturl" : "https://s7g10.scene7.com/is/content/", 
+          "config" : "accorhotelsdev2/Video_social",
+          "posterimage": "accorhotelsdev2/dynamicvideo360-sample-4-AVS",
+          "videoserverurl": "https://s7g10.scene7.com/is/content",
+          "asset" : "accorhotelsdev2/dynamicvideo360-sample-4-AVS" }
+      })
+    s7videoviewer.init(); 
+    </script>
+    </div>
+  `;
 }
-
-function embedVimeo(url, autoplay) {
-  const [, video] = url.pathname.split('/');
-  const suffix = autoplay ? '?muted=1&autoplay=1' : '';
-  return `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-      <iframe src="https://player.vimeo.com/video/${video}${suffix}" 
-      style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
-      frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen  
-      title="Content from Vimeo" loading="lazy"></iframe>
-    </div>`;
-}
-
-function getVideoElement(source, autoplay) {
-  const video = document.createElement('video');
-  video.setAttribute('controls', '');
-  video.dataset.loading = 'true';
-  video.addEventListener('loadedmetadata', () => delete video.dataset.loading);
-  if (autoplay) video.setAttribute('autoplay', '');
-
-  const sourceEl = document.createElement('source');
-  sourceEl.setAttribute('src', source);
-  sourceEl.setAttribute('type', `video/${source.split('.').pop()}`);
-  video.append(sourceEl);
-
-  return video;
-}
-
-const loadVideoEmbed = (block, link, autoplay) => {
-  if (block.dataset.embedIsLoaded) {
-    return;
-  }
-  const url = new URL(link);
-
-  const isYoutube = link.includes('youtube') || link.includes('youtu.be');
-  const isVimeo = link.includes('vimeo');
-  const isMp4 = link.includes('.mp4');
-
-  if (isYoutube) {
-    block.innerHTML = embedYoutube(url, autoplay);
-  } else if (isVimeo) {
-    block.innerHTML = embedVimeo(url, autoplay);
-  } else if (isMp4) {
-    block.textContent = '';
-    block.append(getVideoElement(link, autoplay));
-  }
-
-  block.dataset.embedIsLoaded = true;
-};
 
 export default async function decorate(block) {
-  const placeholder = block.querySelector('picture');
-  const link = block.querySelector('a').href;
-  block.textContent = '';
-
-  if (placeholder) {
-    const wrapper = document.createElement('div');
-    wrapper.className = 'video-placeholder';
-    wrapper.innerHTML = '<div class="video-placeholder-play"><button type="button" title="Play"></button></div>';
-    wrapper.prepend(placeholder);
-    wrapper.addEventListener('click', () => {
-      loadVideoEmbed(block, link, true);
-    });
-    block.append(wrapper);
-  } else {
-    block.classList.add('lazy-loading');
-    const observer = new IntersectionObserver((entries) => {
-      if (entries.some((e) => e.isIntersecting)) {
-        observer.disconnect();
-        loadVideoEmbed(block, link, false);
-        block.classList.remove('lazy-loading');
-      }
-    });
-    observer.observe(block);
-  }
+  block.innerHTML = loadVideoEmbed(block);
+  block.dataset.embedIsLoaded = true;
 }
